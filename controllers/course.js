99cd9fd408aa65c986fb840468b1f8d638fe87d0
pratimagupta98 <pratimadevelopersveltosest@gmail.com>
2022-04-01 17:32:51 +0530

    done

--- controllers/course.js
+++ controllers/course.js
@@ -214,7 +214,7 @@ exports.editcoursebystaff = async (req, res) => {
 
 
 exports.viewonecourse = async (req, res) => {
-  await Course.findOne({ _id: req.sellerId })
+  await Course.findOne({ _id: req.params.id })
     .populate("teacher")
     .then((data) => resp.successr(res, data))
     .catch((error) => resp.errorr(res, error));
