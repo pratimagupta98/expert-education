f117d78d2a4d3d9dc3ff65ee8916ad45d5550976
monikadevelopersveltosest <mailto:monikadevelopersveltosest@gmail.com>
2022-04-12 11:57:52 +0530

    commit

--- controllers/user.js
+++ controllers/user.js
@@ -52,7 +52,7 @@ exports.signup = async (req, res) => {
         );
         res.header("user-token", token).status(200).json({
           status: true,
-          "user-token": token,
+          "token": token,
           msg: "success",
           user: result,
         });
