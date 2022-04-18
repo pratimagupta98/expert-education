5829307286cd022b209178808d2e19e35733a66f
devbhawanasveltose <bhawanadevelopersveltosest14@gmail.com>
2022-04-18 13:06:48 -0700

    change

--- controllers/user.js
+++ controllers/user.js
@@ -51,7 +51,7 @@ exports.signup = async (req, res) => {
         );
         res.header("auth-token", token).status(200).json({
           status: true,
-          "auth-token": token,
+          token: token,
           msg: "success",
           user: result,
         });
