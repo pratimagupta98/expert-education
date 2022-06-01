db15e1f66eecc7680d036dcbe6f4f21da25100b6
pratimagupta98 <pratimadevelopersveltosest@gmail.com>
2022-06-01 18:29:02 +0530

    commit

--- routes/withdrawal.js
+++ routes/withdrawal.js
@@ -5,10 +5,12 @@ const { tokenverify } = require("../functions/tokenverify");
 
 const {
     withdrawal,
+    withdrawal_list
    
 } = require("../controllers/withdrawal");
 
 router.post("/user/withdrawal",tokenverify, withdrawal);
- 
+router.get("/user/withdrawal_list", withdrawal_list);
+
 
 module.exports = router;
