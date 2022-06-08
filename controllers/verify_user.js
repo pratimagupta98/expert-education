5e64b41f76d20d1a088bdf1ac18501e56916843c
pratimagupta98 <pratimadevelopersveltosest@gmail.com>
2022-06-08 15:41:18 +0530

    commit

--- controllers/verify_user.js
+++ controllers/verify_user.js
@@ -1,70 +1,71 @@
 const Verifycode =  require("../models/verify_user");
 const User = require("../models/user");
 
-exports.verify_user = async (req, res) => {
-    const { mobile, otp } = req.body;
-    const dealerDetail = await User.findOne({ mobile: mobile });
-    if (dealerDetail) {
-      if (otp == "123456") {
-        if (dealerDetail.userverified) {
-          const token = jwt.sign(
-            {
-              dealerId: dealerDetail._id,
-            },
-            key,
-            {
-              expiresIn: "365d",
-            }
-          );
-          await Dealershipform.findOneAndUpdate(
-            {
-              _id: req.params.id,
-            },
-            { $set: { userverified: true } },
-            { new: true }
-          ).then((data) => {
-            res.json({
-              status: "success",
-              token: token,
-              msg: "Welcome Back",
-              otpverified: true,
-              redirectto: "dashboard",
-              data: data,
-            });
-          });
-        } else {
-          if (!dealerDetail.userverified) {
-            const token = jwt.sign(
-              {
-                dealerId: dealerDetail._id,
-              },
-              key,
-              {
-                expiresIn: "365d",
-              }
-            );
-            res.json({
-              status: "success",
-              token: token,
-              msg: "Continue signup",
-              otpverified: true,
-              redirectto: "signupdetail",
-            });
-          }
-        }
-      } else {
-        res.json({
-          status: "failed",
-          msg: "Incorrect OTP",
-        });
-      }
-    } else {
-      res.json({
-        status: "error",
-        msg: "User doesnot exist",
-      });
-    }
-  };
+// exports.verify_user = async (req, res) => {
+//     const { verify_user, verify_code } = req.body;
+//     const dealerDetail = await User.findOne({ verify_user: verify_user });
+//     console.log(dealerDetail)
+//     if (dealerDetail) {
+//       if (otp == "123456") {
+//         if (dealerDetail.userverified) {
+//           const token = jwt.sign(
+//             {
+//               dealerId: dealerDetail._id,
+//             },
+//             key,
+//             {
+//               expiresIn: "365d",
+//             }
+//           );
+//           await Dealershipform.findOneAndUpdate(
+//             {
+//               _id: req.params.id,
+//             },
+//             { $set: { userverified: true } },
+//             { new: true }
+//           ).then((data) => {
+//             res.json({
+//               status: "success",
+//               token: token,
+//               msg: "Welcome Back",
+//               otpverified: true,
+//               redirectto: "dashboard",
+//               data: data,
+//             });
+//           });
+//         } else {
+//           if (!dealerDetail.userverified) {
+//             const token = jwt.sign(
+//               {
+//                 dealerId: dealerDetail._id,
+//               },
+//               key,
+//               {
+//                 expiresIn: "365d",
+//               }
+//             );
+//             res.json({
+//               status: "success",
+//               token: token,
+//               msg: "Continue signup",
+//               otpverified: true,
+//               redirectto: "signupdetail",
+//             });
+//           }
+//         }
+//       } else {
+//         res.json({
+//           status: "failed",
+//           msg: "Incorrect OTP",
+//         });
+//       }
+//     } else {
+//       res.json({
+//         status: "error",
+//         msg: "User doesnot exist",
+//       });
+//     }
+//   };
   
 
   //console
--- routes/withdrawal.js
+++ routes/withdrawal.js
@@ -18,7 +18,7 @@ router.get("/admin/withdrawal_list", withdrawal_list);
 router.post("/user/usdt_withdrawal",tokenverify, usdt_withdrawal);
 router.get("/user/pending_withdrwal", pending_withdrwal);
 router.get("/admin/confrm_withdrawal", confrm_withdrawal);
-router.get("/user/cnfrm_withdrawal/:id", cnfrm_withdrawal);
+router.get("/admin/cnfrm_withdrawal/:id", cnfrm_withdrawal);
 
 router.get("/user/dltwithdrwal/:id", dltwithdrwal);
 
