11b6501eb486d7b3c07129934d1a591dd73936a0
pratimagupta98 <pratimadevelopersveltosest@gmail.com>
2022-07-05 13:29:56 +0530

    commit

--- routes/course.js
+++ routes/course.js
@@ -6,9 +6,7 @@ const { verifyToken } = require("../functions/stafftoken");
 const multer = require("multer");
 const {} = require("multer");
 const fs = require("fs");
- const encode = require('node-base64-image').encode
-const decode = require('node-base64-image').decode
-
+  
 
 if (!fs.existsSync("./uploads")) {
   fs.mkdirSync("./uploads");
