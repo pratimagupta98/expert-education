fedb68694609fc789ad7d95bc17f3eb7d9376d91
pratimagupta98 <pratimadevelopersveltosest@gmail.com>
2022-07-11 13:21:56 +0530

    commit

--- controllers/pdffile.js
+++ controllers/pdffile.js
@@ -1,11 +1,27 @@
 const Pdffile = require("../models/pdffile");
 const resp = require("../helpers/apiResponse");
 const { uploadFile } = require("../helpers/awsuploader");
+const { uploadBase64ImageFile } = require("../helpers/awsuploader");
 
 const fs = require("fs");
+var signatures = {
+  JVBERi0: "application/pdf",
+  R0lGODdh: "image/gif",
+  R0lGODlh: "image/gif",
+  iVBORw0KGgo: "image/png",
+  "/9j/": "image/jpg"
+};
+
+function detectMimeType(b64) {
+  for (var s in signatures) {
+    if (b64.indexOf(s) === 0) {
+      return signatures[s];
+    }
+  }
+}
 
 exports.addpdf = async (req, res) => {
-  const { pdf_title, course } = req.body;
+  const { pdf_title, course,pdf_image } = req.body;
 
   // const newPdffile = new Pdffile({
   //   pdf_title: pdf_title,
@@ -27,24 +43,46 @@ exports.addpdf = async (req, res) => {
       course: course,
       teacher: req.staffId,
       // pdf_file: pdf_file,
-      //pdf_image: pdf_image,
+      pdf_image: pdf_image,
 
       //category: categoryId,
     });
 
-    if (req.files) {
-      console.log(req.files);
-      if (req.files.pdf_image) {
-        const geturl = await uploadFile(
-          req.files.pdf_image[0]?.path,
-          req.files.pdf_image[0]?.filename,
-          "jpg"
+    // if (req.files) {
+    //   console.log(req.files);
+    //   if (req.files.pdf_image) {
+    //     const geturl = await uploadFile(
+    //       req.files.pdf_image[0]?.path,
+    //       req.files.pdf_image[0]?.filename,
+    //       "jpg"
+    //     );
+    //     if (geturl) {
+    //       newPdffile.pdf_image = geturl.Location;
+    //       //fs.unlinkSync(`../uploads/${req.files.course_image[0]?.filename}`);
+    //     }
+    //   }
+    if (pdf_image) {
+      if (pdf_image) {
+        
+
+        const base64Data = new Buffer.from(pdf_image.replace(/^data:image\/\w+;base64,/, ""), 'base64');
+        detectMimeType(base64Data);
+        const type = detectMimeType(pdf_image);
+        // console.log(newCourse,"@@@@@");
+        const geturl = await uploadBase64ImageFile(
+          base64Data,
+          newPdffile.id,
+         type
         );
+        console.log(geturl,"&&&&");
         if (geturl) {
           newPdffile.pdf_image = geturl.Location;
+         
           //fs.unlinkSync(`../uploads/${req.files.course_image[0]?.filename}`);
         }
       }
+
+    
       if (req.files.pdf_file && req.files.pdf_image) {
         for (let i = 0; i < req.files.pdf_file.length; i++) {
           const getpdfurl = await uploadFile(
